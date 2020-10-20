bcf72bad775f2ef3a341f267fb471922644dd66f
Evelin Rodriguez Suarez <evelin992009@gmail.com>
2020-10-20 20:40:16 +0000

    4 tipos de funciones

--- main.py
+++ main.py
@@ -10,5 +10,27 @@ def funcion02(a,b):
   v = a * b
   print(v)
 
+
+#funcion que no recibe nada, pero retorna dos valores
+
+def funcion03():
+  a = 4
+  b = 5
+  return a, b
+
+
 funcion01()
 funcion02(3 , 7)
+gA , gB= funcion03 ()
+print (gA)
+print (gB)
+
+#funcion con dos parametros y retorna 1 valor
+ 
+def funcion04(a,b):
+  v = a * b
+  return v
+
+gC = funcion04 (gA,gB)
+print (gC)
+ 
