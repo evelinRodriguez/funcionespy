1b48b275305ce0443c179d5f1becbab20bb205e1
Evelin Rodriguez Suarez <evelin992009@gmail.com>
2020-10-20 20:28:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#funcion no recibe ni retorna
+def funcion01():
+  c = 2 * 6
+  print(c)
+ 
+
+  # funcion con dos parametros
+
+def funcion02(a,b):
+  v = a * b
+  print(v)
+
+funcion01()
+funcion02(3 , 7)
